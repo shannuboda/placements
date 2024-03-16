bf1b58e7cee8eea441484026d38e579cc651bdf2
shannuboda <shanmukha.boda@gmail.com>
2024-03-16 22:39:34 +0530

    New Header Change Occur boy

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Header</title>
+    <title>Header New</title>
 
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
