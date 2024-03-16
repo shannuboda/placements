70fc579385280333deb710fa071f3ded67c65952
shannuboda <69586979+shannuboda@users.noreply.github.com>
2024-03-16 22:42:31 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Header New</title>
+    <title>Header New college</title>
 
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
